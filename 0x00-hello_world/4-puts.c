13d53cbd30ef7463d925993329e2faf72716448c
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-08 21:09:37 +0100

    print using the puts fucntion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*print "programming is like building a multilingual puzzle"
+ * always return 0
+ */
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
