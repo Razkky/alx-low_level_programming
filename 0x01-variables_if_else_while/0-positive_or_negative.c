1345a22ef1b92dd9fb7d5775eaac4c7027b26ca5
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-09 23:01:08 +0100

    if and else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * main - Entry point
+/**
+ * main - determine if number is positive or negative
  *
- * Description: prints "with proper grammar, but the outcome is a piece of
- * art,"
+ * Description: program will assign a random number to the variable n each time
+ * it is executed.
  *
  * Return: Always 0 (Success)
  */
@@ -17,10 +17,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 	else if (n == 0)
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	else
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 	return (0);
 }
