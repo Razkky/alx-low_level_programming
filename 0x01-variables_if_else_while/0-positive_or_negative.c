bbcb478771e5f3bc7292cf45e257620bc52f7e48
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-09 22:49:03 +0100

    first if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("is negative");
+	else if (n == 0)
+		printf("is zero");
+	else
+		printf("is negative");
+	return (0);
+}
