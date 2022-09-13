d4b639cfbaf644dc16192f97e966bf6762b3b2a6
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-13 17:36:36 +0100

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Description: print alphabet in lowercase 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 'a';
+		_putchar('\n');
+	}
+}
