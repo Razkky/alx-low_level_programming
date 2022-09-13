ac65e8e5aface77760786603d16681edba26d807
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-13 23:38:00 +0100

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add two numbers
+ *@n: number to be added
+ *@b: number to be added
+ *
+ *Description: add two numbers and return answer
+ *
+ * Return: Answer
+ */
+
+int add(int n, int b)
+{
+	return (n + b);
+}
