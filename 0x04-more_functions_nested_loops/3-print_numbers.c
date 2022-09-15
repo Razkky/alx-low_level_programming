53f2bc0b31d0d75a7a331a7b9bd635d4e6d75156
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-15 21:16:42 +0100

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print number
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 49;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
