d22d1c1de1b9911b94cd5af8c5c18fc73026db28
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-15 21:42:37 +0100

    print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i);
+		i++;
+	}
+}
