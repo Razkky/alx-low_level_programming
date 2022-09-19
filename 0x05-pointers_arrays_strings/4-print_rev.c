6f79c16fbb47c1c637c9a46780f899914850fc79
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-19 18:34:01 +0100

    print in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
