ae2e7a9c9a249f7b640756cc43431f68bc588687
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-19 21:48:23 +0100

    fix error

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
-	len = strlen(src);
-	for (i = 0; i < len; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = *(src + i);
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
