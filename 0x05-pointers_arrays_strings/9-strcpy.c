675624a4c1aa469f5c2c9fbf45290b0acb2b832d
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-19 21:44:16 +0100

    fix betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,10 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = strlen(src);
-	for (i = 0; i < len; i++){
-		*(dest + i) = *(src +i);
+	for (i = 0; i < len; i++)
+	{
+		*(dest + i) = *(src + i);
 	}
 
-	return dest;
+	return (dest);
 }
