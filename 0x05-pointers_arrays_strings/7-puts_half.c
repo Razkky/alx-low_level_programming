58654d6095ee64fef92851441f1d4c7bdf95bf34
Muhammad jamiu Abdul Razak <muhammadjamiuabdulrazak@gmial.com>
2022-09-19 21:05:02 +0100

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - print half string
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = strlen(str);
+	j = (i-1)/2 + 1;
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
