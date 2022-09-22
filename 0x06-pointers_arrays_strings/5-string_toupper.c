044816a8fd9c800b6cfc3ef6a194fe5fca162b91
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-22 17:18:52 +0100

    change string to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+/**
+ * string_toupper - change string to upper case
+ * @a: inputed string
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+	char c;
+	char *b = a;
+
+	i = 0;
+	for (; *(b + i) != '\0'; i++)
+	{
+		c = *(b + i);
+		if (c > 96 && c < 123)
+		{
+			c = c - 32;
+			*(a + i) = c;
+		}
+	}
+	return (a);
+}
