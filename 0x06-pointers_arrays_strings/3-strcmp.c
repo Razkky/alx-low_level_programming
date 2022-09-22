86a2641dde1ed49afa92ef62db1a9bd02f63eb68
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-22 15:45:01 +0100

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: 1 or -1 or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, answer;
+
+	for (; *s1+i != '\n'; i++)
+	{
+		answer = (*s1 + i) - (*s2 + i);
+		if (answer != 0)
+			return (answer);
+	}
+	return (0);
+}
