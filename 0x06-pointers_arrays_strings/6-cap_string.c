cfacc79fdbab9a405abee27297e4d6a0255fbedb
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-22 17:55:56 +0100

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+/**
+ * cap_string - capitalize first word of a string
+ * @a: string in reference
+ *
+ * Return: string
+ */
+
+char *cap_string(char *a)
+{
+	int n;
+	char c;
+
+	n = 0;
+	for (; *(a + n) != '\0'; n++)
+	{
+		if (n == 0 || *(a + n -1) == 32)
+		{
+			if (*(a + n) > 96 && *(a + n) < 123)
+			{
+				c = *(a + n);
+				c = c - 32;
+				*(a + n) = c;
+			}
+		}
+	}
+	return (a);
+}
