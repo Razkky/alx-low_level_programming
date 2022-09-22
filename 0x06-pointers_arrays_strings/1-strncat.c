06f47a807ddc0ba138ff732cf4d32a33da5c67e6
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-22 12:11:18 +0100

    fix error

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 	;
 	for (; *src != '\0'; src++)
 	{
+		*pointer = *src;
+		n--;
 		if (n == 0)
 			break;
-		*pointer = *src;
 		pointer++;
-		n--;
 	}
 	*pointer = *src;
 	return (dest);
