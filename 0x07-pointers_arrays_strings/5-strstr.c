c88eac5f44fdcd9ae8a03b11b9865b0686e467d8
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-27 00:15:10 +0100

    fix betty code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,20 @@
 #define NULL 0
 /**
- * _strstr - find occurance of string b in string a
- * @haystack: string a
- * @needle: string b
+ * _strstr - find substrig
+ * @haystack: first string
+ * @needle: second string
  *
- * Return: Location of location of string b in a
+ * Return: char
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k, len, count;
+	int i, j;
 
-	i = 0;
-	k = 0;
-	for (len = 0; needle[len] != '\0'; len++)
-	;
-	while (*(haystack + i) != '\0')
+	j = 0;
+	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		if (*(haystack + i) == *(needle + k))
-		{
-			j = i;
-			count = 0;
-			k = 0;
-			while (*(haystack + j) != ' ' && *(haystack + j) != '\0')
-			{
-				if (*(haystack + j) == *(needle + k))
-					count++;
-				j++;
-				k++;
-			}
-			k = 0;
-			if (count == len)
-				return (haystack + i);
-		}
-		i++;
+		if (*(haystack + i) == *(needle + j))
+			return (haystack + i);
 	}
 	return (NULL);
 }
