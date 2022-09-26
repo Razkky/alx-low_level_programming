11b233b1c7c1ec95608f08799c443dd2afaf8185
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-09-27 00:16:42 +0100

    rename file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,39 @@
 #define NULL 0
 /**
- * _strstr - find substrig
- * @haystack: first string
- * @needle: second string
+ * _strstr - find occurance of string b in string a
+ * @haystack: string a
+ * @needle: string b
  *
- * Return: char
+ * Return: Location of location of string b in a
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, k, len, count;
 
-	j = 0;
-	for (i = 0; *(haystack + i) != '\0'; i++)
+	i = 0;
+	k = 0;
+	for (len = 0; needle[len] != '\0'; len++)
+	;
+	while (*(haystack + i) != '\0')
 	{
-		if (*(haystack + i) == *(needle + j))
-			return (haystack + i);
+		if (*(haystack + i) == *(needle + k))
+		{
+			j = i;
+			count = 0;
+			k = 0;
+			while (*(haystack + j) != ' ' && *(haystack + j) != '\0')
+			{
+				if (*(haystack + j) == *(needle + k))
+					count++;
+				j++;
+				k++;
+			}
+			k = 0;
+			if (count == len)
+				return (haystack + i);
+		}
+		i++;
 	}
 	return (NULL);
 }
