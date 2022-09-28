71b69a44fc88b1adbcb0fbff9fe2c140d77804ce
root <root@DESKTOP-MIUPKIG.localdomain>
2022-09-28 20:55:45 +0100

    find factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - calculate factorial of a number
+ * @n: inputed integer
+ *
+ * Return: n factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
