686a012fe52eab57d53de49f96204faf6e5609c5
root <root@DESKTOP-MIUPKIG.localdomain>
2022-09-28 21:17:13 +0100

    calculate the power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _pow_recursion - return value of x^y
+ * @x: integer
+ * @y: power
+ *
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
