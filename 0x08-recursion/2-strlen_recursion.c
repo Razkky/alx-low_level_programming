f8032fc34930fa101f76d2d8b9504c5a8a061a28
root <root@DESKTOP-MIUPKIG.localdomain>
2022-09-28 20:44:44 +0100

    fix error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*(s + 1) == '\0')
+	if (*s != '\0')
 	{
-		return 1;
+		return 1 + _strlen_recursion((s + 1));
 	}
-	return 1 + _strlen_recursion(s + 1);
+	else
+	{
+		return (0);
+	}
 }
