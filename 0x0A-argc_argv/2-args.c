4fe23a512c89915e3ec6c2c01199fbbf4db9e5cf
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-01 13:32:12 +0100

    fix betty error

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,15 +3,15 @@
  * main - print value of arguement
  * @args: number of arguement
  * @argv: value of arguement
- * return: sucess (0)
+ * Return: sucess (0)
  */
-int main(__attribute__((unused)) int args, char *argv[])
+int main(int args, char *argv[])
 {
 	int i = 0;
-	
+
 	for (; i < args; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
 	return (0);
-}	
+}
