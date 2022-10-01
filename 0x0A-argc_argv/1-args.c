1b95b1d49b587eba41db37a111a6446bdf1a7246
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-01 13:19:12 +0100

    print number of arguement

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print number of arguement 
+ * @argc: number of arguement
+ * @argv: value of arguement
+ *
+ * Return: sucess (0)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
