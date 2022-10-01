bc93c0a13c8ead81430d789fc0046455f7cbfafa
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-01 13:42:18 +0100

    multiple two argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two argument
+ * @args: number of argument
+ * @argv: value of argument
+ * Return: Multiple of two arguement or error
+ */
+int main(int args, char *argv[])
+{
+	int i = 1, multiple = i;
+
+	if (args < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (; i < args; i++)
+		{
+			multiple = multiple * atoi(argv[i]);
+		}
+		printf("%d\n", multiple);
+	}
+	return (0);
+}
