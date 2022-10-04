1caf6d259bdf372cc9a889572209a3ba8f5b13d3
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-05 00:11:45 +0100

    create array and initialize it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - create an array and initialize it with char c
+ * @size: size of array
+ * @c: value to inititalizing array
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *k;
+	unsigned int i = 0;
+
+	k = malloc(size * sizeof(char));
+	for (; i < size ; i++)
+	{
+		k[i] = c;
+	}
+	return (k);
+}
