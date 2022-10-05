ba144cd0768a4cf28b6da828ce6201d8feadd0ad
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-05 02:04:44 +0100

    restore file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,39 +9,41 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *l;
-	int len = 0, i = 0, k = 0, len1 = 0, len2 = 0;
+	int len, i, k;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	i = 0;
 	while (*(s1 + i) != '\0')
 	{
 		len++;
-		len1++;
 		i++;
 	}
 	i = 0;
 	while (*(s2 + i) != '\0')
 	{
 		len++;
-		len2++;
 		i++;
 	}
+
 	l = malloc((len + 1) * sizeof(char));
 	if (l == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len1)
+	while (*(s1 + i) != '\0')
 	{
 		l[i] = s1[i];
 		i++;
 	}
 	k = i;
-	for (i = 0; i < len2; i++)
+	for (i = 0; *(s2 + i) != '\0'; i++)
 	{
 		l[k] = s2[i];
 		k++;
 	}
+
 	return (l);
 }
