99fbee6d9fb4ce0d7611d4a050e2d3be8c0b2ade
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-05 01:27:19 +0100

    fix return error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,14 +3,14 @@
 /**
  * str_concat - concatinate s1 to s2
  * @s1: first stirng
- * @s@: second string
+ * @s2: second string
  * Return: pointer to concatednated string
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *l;
 	int len, i, k;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -20,27 +20,30 @@ char *str_concat(char *s1, char *s2)
 	while (*(s1 + i) != '\0')
 	{
 		len++;
+		i++;
 	}
 	i = 0;
 	while (*(s2 + i) != '\0')
 	{
 		len++;
+		i++;
 	}
-	printf("%d\n",len);
 
 	l = malloc((len + 1) * sizeof(char));
 	if (l == NULL)
 		return (NULL);
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	i = 0;
+	while (*(s1 + i) != '\0')
 	{
 		l[i] = s1[i];
+		i++;
 	}
 	k = i;
 	for (i = 0; *(s2 + i) != '\0'; i++)
 	{
 		l[k] = s2[i];
+		k++;
 	}
-	l[k] = '\0';
-	
+
 	return (l);
 }
