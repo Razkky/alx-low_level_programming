e76fdee7de5d2af43fd6427222ce7f847116dcd8
Abdul-Razak <Muhammadjamiuabdulrazak@gmail.com>
2022-10-06 21:35:51 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,45 +9,23 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *pointer;
-	char *temp;
-	char *old;
-	unsigned int i;
-
-	old = ptr;
-
-	if (new_size == old_size)
-		return (ptr);
 	if (ptr == NULL)
 	{
-		pointer = malloc(new_size * sizeof(int));
+		ptr = malloc(new_size);
 		return (ptr);
 	}
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
-	{
-		pointer = malloc(new_size * sizeof(int));
-		temp = pointer;
-		for (i = 0; i < old_size; i++)
-		{
-			temp[i] = old[i];
-		}
-		return (pointer);
-	}
-	if (new_size < old_size)
-	{
-		pointer = malloc(new_size * sizeof(int));
-		temp = pointer;
-		for (i = 0; i < new_size; i++)
-		{
-			temp[i] = old[i];
-		}
-		return (pointer);
-	}
+
+	if (new_size == old_size)
+		return (ptr);
 	free(ptr);
-	return (NULL);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
