b085299a332f25305e80711e6cbcff4de6d9337e
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-07 11:48:04 +0100

    create 2D array using malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid, i;
+	int **grid, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -24,14 +24,10 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
-		{
-			while (i >= 0)
-			{
-				free(grid[i]);
-				i--;
-			}
-			free(grid);
 			return (NULL);
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
 		}
 	}
 	return (grid);
