bdee36838109002774de6f70d5fd9754403038ef
root <root@DESKTOP-MIUPKIG.localdomain>
2022-10-07 11:58:44 +0100

    free grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,20 @@ int **alloc_grid(int width, int height)
 	grid =  (int **) malloc(height * sizeof(int *));
 
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			free(grid[i]);
+			for(; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
