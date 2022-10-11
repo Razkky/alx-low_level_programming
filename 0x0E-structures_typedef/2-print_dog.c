5a713bcdfd415c5184a39adda58d4605918889fa
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-11 14:51:54 +0100

    add else statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,12 +10,12 @@ void print_dog(struct dog *d)
 	{
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
-		if (d->name == NULL)
+		else
 			printf("Name: (ni)l\n");
 		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
-		if (d->owner == NULL)
+		else
 			printf("Owner: (nil)\n");
 	}
 }
