9e7cf3ae862d11d35a3b26a1b4c5d3f9428bd132
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-11 14:53:30 +0100

    correct spelling

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (ni)l\n");
+			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
