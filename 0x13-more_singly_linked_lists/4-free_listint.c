65fa878fe90565169604852d964424390d8c317b
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-25 11:39:35 +0100

    free memory blocks

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free malloc allocated space to list
+ * @head: pointer to head of a list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head->next != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->next);
+		free(temp);
+	}
+}
