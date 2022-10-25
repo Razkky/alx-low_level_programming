85afa8c280b4839c54425aecf8ce60b6a8175fa2
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-25 11:46:41 +0100

    fix error

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
-	free(head);
 }
