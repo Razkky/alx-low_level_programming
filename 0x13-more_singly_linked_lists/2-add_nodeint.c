00a1a3aff5eb13b797b74ba9f7b768ada9ce4465
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-25 10:27:04 +0100

    add node at begining of a lsit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+*add_nodeint - add node at the begining of a list
+*@head: pointer to the head pointer of the list
+*@n: variable to be added to newly created list
+*Return: pointer to newly created list
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp, *new;
+
+	new = malloc(sizeof(n));
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	*head = new;
+	new->next = temp;
+	return (new);
+}
