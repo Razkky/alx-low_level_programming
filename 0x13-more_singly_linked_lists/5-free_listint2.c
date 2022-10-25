57806d3f3ddffeb5b99a2d9d8ef9d52b5238aeb9
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-25 12:06:23 +0100

    free space and point head to nnull

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - free malloc allocated space to list
+ * @head: pointer to head of a list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *point = *head;
+
+	while (point != NULL)
+	{
+		temp = point;
+		point = point->next;
+		free(temp);
+	}
+	*head = NULL;
+}
