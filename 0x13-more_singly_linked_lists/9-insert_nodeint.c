da9eeb6895c0af44c520d4b5b519bca3c6fd8f34
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-26 11:13:02 +0100

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		index++;
 	}
-	if (idx >= index)
+	if (idx > index)
 		return (NULL);
 	index = 0;
 	temp = *head;
