57d6cf708eec65729dd827408d3c321ab086ff9e
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-27 15:19:55 +0100

    convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,45 @@
 #include "main.h"
 /**
  *power - calculate the exponential of a base
- *base: base of a number
- *exp: exponential of a number
+ *@base: base of a number
+ *@exp: exponential of a number
  *Return: result
  */
 unsigned int power(unsigned int base, unsigned int exp)
 {
 	unsigned int result = 1;
+
 	while (exp != 0)
 	{
 		result *= base;
+		exp--;
 	}
 	return (result);
 }
 
 /**
- * binary_to_unit - convert binary to base 10
+ * binary_to_uint - convert binary to base 10
  * @b: pointer to a string of 0 and 1
  * Return: base 10 form of binary string
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int b10;
-	int count = 0, i = 0;
+	unsigned int b10 = 0, count = 0;
+	int i = 0;
 
-	while (b[i] != NULL)
+	while (*(b + i) != '\0')
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		count++;
 		i++;
 	}
 	i = 0;
 	count--;
-	while (b[i] != NULL)
+	while (b[i] != '\0')
 	{
-		if (b[i] = '1')
+		if (b[i] == '1')
 		{
 			b10 += power(2, count);
 		}
@@ -50,4 +52,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (b10);
 }
-	
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,4 +3,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
+unsigned int power(unsigned int base, unsigned int exp);
 #endif /* End MAIN_H */
