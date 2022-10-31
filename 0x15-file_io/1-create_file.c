34d2e329b85b2cfcd2ca167b71df7d14c35aecc5
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-31 20:45:32 +0100

    trucate if file already exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
 		w = write(fd, text_content, strlen(text_content));
 	if (fd == -1 || w == -1)
