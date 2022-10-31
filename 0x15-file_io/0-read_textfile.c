20f49df350072d5237f22439a28503503960030b
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-10-31 16:00:38 +0100

    change malloc return value

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 	{
 		free(buff);
-		return (-1);
+		return (0);
 	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
