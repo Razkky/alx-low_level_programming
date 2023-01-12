a060c30bb6f6d8cc53928bf7158208f186b28a9f
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-12 08:25:28 +0100

    create hash table and return pointer to table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of the table
+ * Return: pointer to the newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		free(table);
+		return NULL;
+	}
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t*));
+	if (table->array == NULL)
+	{
+		free(table->array);
+		free(table);
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
