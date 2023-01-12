caa8030f7753a5ea1095063b69696e29b15e41bc
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-12 08:32:37 +0100

    fix betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 	{
 		free(table);
-		return NULL;
+		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t*));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table->array);
 		free(table);
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
