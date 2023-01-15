2cf085e25fe97e47c6ba04e2e88545b7ee67301a
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-15 14:22:29 +0100

    fix output error

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 			for (node = ht->array[i]; node != NULL; node = node->next)
 			{
-				printf("'%s' : '%s'", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				if (node->next != NULL)
 					printf(", ");
 			}
