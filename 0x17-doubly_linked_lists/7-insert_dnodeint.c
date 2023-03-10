18e900927c15f7308fa562730c5760e55fc3fe4b
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-10 23:05:14 +0100

    add element at an index in a linked list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get the nth node of a linked list
+ * @head: pointer to the head of a linked list
+ * @index: the index in refrence
+ * Return: pointer the the nth node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *temp, *new;
+
+	temp = *head;
+	while (i < index - 1 && temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next = new;
+
+	return (new);
+}
