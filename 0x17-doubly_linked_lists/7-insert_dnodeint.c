37a44ca7739c7f0aa4c6266a4f19121ed9274262
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-10 23:08:08 +0100

    fix betty error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,10 +3,12 @@
  * get_dnodeint_at_index - get the nth node of a linked list
  * @head: pointer to the head of a linked list
  * @index: the index in refrence
+ * @n: elemetn to be added
  * Return: pointer the the nth node
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
+		unsigned int index, int n)
 {
 	unsigned int i = 0;
 	dlistint_t *temp, *new;
