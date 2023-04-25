70248b6c98e19cee4815dba778b605412e36731d
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-25 17:42:35 +0100

    read from a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filename = process.argv[2];
+fs.readFile(filename, 'utf8', (error, file) => {
+  if (error) throw error;
+  console.log(file);
+});
